7e50b681b3a880db2b3a44bcb0c110fd6452642c
PavanAlagani <pavankumaralagani@gmail.com>
2022-12-24 04:57:48 +0530

    sample commit

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ print("Starting python learning")
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+print("i learnt how to print output in python")
+
