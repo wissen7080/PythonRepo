3b90ccae07b15fadd0f5573f1b339efee09104cd
PavanAlagani <pavankumaralagani@gmail.com>
2022-12-24 04:58:55 +0530

    Revert "sample commit"
    This reverts commit 7e50b681b3a880db2b3a44bcb0c110fd6452642c.

--- app.py
+++ app.py
@@ -2,5 +2,3 @@ print("Starting python learning")
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
-print("i learnt how to print output in python")
-
